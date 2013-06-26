85372f6a117861aab19a196627aeeb23e923a7ca
Mike Bielagus <bielagus@gmail.com>
2013-06-26 16:15:50 -0400

    Validate that fields are non-empty

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,12 +1,16 @@
 class RestaurantsController < ApplicationController
 	def new
+		@restaurant = Restaurant.new
 	end
 
 	def create
 		@restaurant = Restaurant.new(params[:restaurant])
 
-		@restaurant.save
-		redirect_to @restaurant
+		if @restaurant.save
+			redirect_to @restaurant
+		else
+			render 'new'
+		end
 	end
 
 	def show
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,7 @@
 class Restaurant < ActiveRecord::Base
   attr_accessible :address, :description, :name, :phone
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :address, presence: true
+  validates :phone, presence: true
 end
