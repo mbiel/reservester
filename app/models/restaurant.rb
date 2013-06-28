fcff3c5d634b7646d1bd84bf29caffac25fc98b4
Mike Bielagus <bielagus@gmail.com>
2013-06-28 15:23:35 -0400

    merged pdf

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,9 +1,10 @@
 class Restaurant < ActiveRecord::Base
-  attr_accessible :address, :description, :name, :phone, :image, :image_cache
+  attr_accessible :address, :description, :name, :phone, :image, :image_cache, :menu, :menu_cache
   validates :name, presence: true
   validates :description, presence: true
   validates :address, presence: true
   validates :phone, presence: true
 
   mount_uploader :image, ImageUploader
+  mount_uploader :menu, MenuUploader
 end
