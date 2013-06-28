c0c578f74d78bfdde62073bcecc82f9a0043c55e
Mike Bielagus <bielagus@gmail.com>
2013-06-27 23:17:32 -0400

    added destroy action

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -35,5 +35,12 @@ class RestaurantsController < ApplicationController
 		end
 	end
 
+	def destroy
+		@restaurant = Restaurant.find(params[:id])
+		@restaurant.destroy
+
+		redirect_to restaurants_path
+	end
+
 	
 end
