fdb756e6ea9bfbc073850fd1a61ec10f1d505b03
Mike Bielagus <bielagus@gmail.com>
2013-07-01 11:48:09 -0400

    restaurant belongs to owner

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ActiveRecord::Base
-  attr_accessible :address, :description, :name, :phone, :image, :image_cache, :menu, :menu_cache, :latitude, :longitude
+  attr_accessible :address, :description, :name, :phone, :image, :image_cache, :menu, :menu_cache, :latitude, :longitude, :owner
   validates :name, presence: true
   validates :description, presence: true
   validates :address, presence: true
@@ -7,6 +7,8 @@ class Restaurant < ActiveRecord::Base
 
   geocoded_by :address
   after_validation :geocode
+
+  belongs_to :owner
 
   mount_uploader :image, ImageUploader
   mount_uploader :menu, MenuUploader
