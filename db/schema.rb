6ec5ef06e4ae17324b26f19e1a542bfded50178c
Mike Bielagus <bielagus@gmail.com>
2013-07-01 12:58:00 -0400

    owner-restaurant migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130701150050) do
+ActiveRecord::Schema.define(:version => 20130701162534) do
 
   create_table "owners", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(:version => 20130701150050) do
     t.string   "menu"
     t.float    "latitude"
     t.float    "longitude"
+    t.integer  "owner_id"
   end
 
 end
